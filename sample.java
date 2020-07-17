1552c49135f81150b62b4dd31d2b04a89519ae7d
EAD\medula <mahesh.edula@dxc.com>
2020-07-17 14:36:44 +0530

    updated salesorder.xml file

--- sample.java
+++ sample.java
@@ -1,7 +1,3 @@
-my first 10 lines
-
-my 2nd 10 lines
-
-my 3rd 10 lines
-
-my 4th 10 lines
+sum(detail.net_del_units_6wk) as net_del_units_6wk,
+        sum(stale.stale_units_prev_13wk) as stale_units_prev_13wk,
+        sum(stale.sales_units_prev_13wk) as sales_units_prev_13wk
